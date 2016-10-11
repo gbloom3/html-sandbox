6677dc25badbfd400f3c7ca28011c4db094a1dfb
Greg Bloom <gbloom3@cnm.edu>
2016-10-11 13:39:05 -0600

    Added heisenberg.png Added ipsum to body in index.php

---
+++ public_html/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang = "eng">
+	<head>
+		<meta charset="utf-8"/>
+		<title>HTML Sandbox</title>
+	</head>
+	<body>
+
+		<header>
+			<h1>Hi! This is my page</h1>
+			<hr/>
+		</header>
+
+		<main>
+			<section>
+				<p>Anything suspicious? Well... then should we go? Any uh... Cartel news these days? Seems like I'm always reading something or other in the paper. I don't want to talk
+about it. To you or anyone else. I'm done explaining myself. Gus is dead. We've got work to do. Jesse Jackson? Do you even... ah, I see you have a telephone at least. You know that blinking thing I've been calling you on? I will break this, I will BREAK THIS. Damn druggie idiot. Is this what you've been doing the whole time I've been trying to reach you? The game has changed. The word is out. And you... are a killer. Apparently it's all over town. Somebody crossed you, you got angry, you crushed their skull with an ATM machine. Who cares! Just as long as it's our competitors who believe it and not the police. Don't you see how great this is? You, you are a... Jesse look at me. You... are a blowfish. A blowfish! Think about it. Small in stature, not swift, not cunning. Easy prey for predators but the blowfish has a secret weapon doesn't he. Doesn't he? What does the blowfish do, Jesse. What does the blowfish do? The blowfish puffs up, okay? </p>
+				<img src="img/heisenberg-blue.png" alt="Heisenberg"/>
+			</section>
+
+			<section>
+
+			</section>
+
+			<section>
+
+			</section>
+
+			<table>
+				<tr>
+					<th>Column 1 Heading</th>
+					<th>Column 2 Heading</th>
+					<th>Column 3 Heading</th>
+				</tr>
+				<tr>
+					<td>Column 1, Row 1</td>
+					<td>Column 2, Row 1</td>
+					<td>Column 3, Row 1</td>
+				</tr>
+				<tr>
+					<td>Column 1, Row 2</td>
+					<td>Column 2, Row 2</td>
+					<td>Column 3, Row 2</td>
+				</tr>
+			</table>
+		</main>
+	</body>
+</html>

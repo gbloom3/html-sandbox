826db3a6c0710b6e680feb8245d55469ecd383de
Greg Bloom <gbloom3@cnm.edu>
2016-10-11 11:53:19 -0600

    Added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "eng">
+	<head>
+		<meta charset="utf-8"/>
+		<title>HTML Sandbox</title>
+	</head>
+	<body>
+		<h1>Hello World!</h1>
+		<table>
+			<tr>
+				<th>Column 1 Heading</th>
+				<th>Column 2 Heading</th>
+				<th>Column 3 Heading</th>
+			</tr>
+			<tr>
+				<td>Column 1, Row 1</td>
+				<td>Column 2, Row 1</td>
+				<td>Column 3, Row 1</td>
+			</tr>
+			<tr>
+				<td>Column 1, Row 2</td>
+				<td>Column 2, Row 2</td>
+				<td>Column 3, Row 2</td>
+			</tr>
+		</table>
+	</body>
+</html>

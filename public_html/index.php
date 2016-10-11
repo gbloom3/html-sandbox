3092731ebbcebaf6561177adff55bfe61cf659d0
Greg Bloom <gbloom3@cnm.edu>
2016-10-11 13:58:13 -0600

    Changed image to hodor.jpg Added an unordered list to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -13,36 +13,44 @@
 
 		<main>
 			<section>
-				<p>Anything suspicious? Well... then should we go? Any uh... Cartel news these days? Seems like I'm always reading something or other in the paper. I don't want to talk
-about it. To you or anyone else. I'm done explaining myself. Gus is dead. We've got work to do. Jesse Jackson? Do you even... ah, I see you have a telephone at least. You know that blinking thing I've been calling you on? I will break this, I will BREAK THIS. Damn druggie idiot. Is this what you've been doing the whole time I've been trying to reach you? The game has changed. The word is out. And you... are a killer. Apparently it's all over town. Somebody crossed you, you got angry, you crushed their skull with an ATM machine. Who cares! Just as long as it's our competitors who believe it and not the police. Don't you see how great this is? You, you are a... Jesse look at me. You... are a blowfish. A blowfish! Think about it. Small in stature, not swift, not cunning. Easy prey for predators but the blowfish has a secret weapon doesn't he. Doesn't he? What does the blowfish do, Jesse. What does the blowfish do? The blowfish puffs up, okay? </p>
-				<img src="img/heisenberg-blue.png" alt="Heisenberg"/>
+				<p>Hodor, hodor. Hodor. Hodor, hodor; hodor hodor hodor hodor? Hodor. Hodor hodor; hodor hodor hodor hodor?! Hodor hodor, hodor. Hodor hodor - hodor, hodor. Hodor hodor hodor! Hodor! Hodor hodor, hodor hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor hodor hodor - hodor hodor hodor! Hodor. Hodor hodor... Hodor hodor hodor, hodor. Hodor hodor. Hodor! Hodor hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor, hodor. Hodor hodor; hodor hodor, hodor, hodor hodor.</p>
+				<img src="img/hodor.jpg" alt="Hodor hodor hodor hodor"/>
 			</section>
 
 			<section>
-
+				<h2>My Favorite Things</h2>
+				<ul>
+					<li>Computers</li>
+					<li>Cameras</li>
+					<li>Programming</li>
+					<li>Donuts</li>
+				</ul>
 			</section>
 
 			<section>
+				<table>
+					<tr>
+						<th>Column 1 Heading</th>
+						<th>Column 2 Heading</th>
+					</tr>
 
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Star Trek</td>
+					</tr>
+				</table>
 			</section>
 
-			<table>
-				<tr>
-					<th>Column 1 Heading</th>
-					<th>Column 2 Heading</th>
-					<th>Column 3 Heading</th>
-				</tr>
-				<tr>
-					<td>Column 1, Row 1</td>
-					<td>Column 2, Row 1</td>
-					<td>Column 3, Row 1</td>
-				</tr>
-				<tr>
-					<td>Column 1, Row 2</td>
-					<td>Column 2, Row 2</td>
-					<td>Column 3, Row 2</td>
-				</tr>
-			</table>
 		</main>
 	</body>
 </html>

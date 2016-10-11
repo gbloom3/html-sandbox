290b160e2bf686f730c7f8541dfec931cdf5bda5
Greg Bloom <gbloom3@cnm.edu>
2016-10-11 15:18:19 -0600

    Added stylesheet.css Added comments and linked the stylesheet

--- public_html/index.php
+++ public_html/index.php
@@ -2,21 +2,28 @@
 <html lang = "eng">
 	<head>
 		<meta charset="utf-8"/>
+
+		<!-- add css file -->
+		<link href="css/stylesheet.css" rel="" type="" />
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
 
+		<!-- this is the header -->
 		<header>
 			<h1>Hi! This is my page</h1>
 			<hr/>
 		</header>
 
+		<!-- this is the main content area -->
 		<main>
+			<!-- begin section 1 -->
 			<section>
 				<p>Hodor, hodor. Hodor. Hodor, hodor; hodor hodor hodor hodor? Hodor. Hodor hodor; hodor hodor hodor hodor?! Hodor hodor, hodor. Hodor hodor - hodor, hodor. Hodor hodor hodor! Hodor! Hodor hodor, hodor hodor hodor... Hodor hodor hodor?! Hodor hodor HODOR! Hodor hodor hodor - hodor hodor hodor! Hodor. Hodor hodor... Hodor hodor hodor, hodor. Hodor hodor. Hodor! Hodor hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor, hodor. Hodor hodor; hodor hodor, hodor, hodor hodor.</p>
 				<img src="img/hodor.jpg" alt="Hodor hodor hodor hodor"/>
 			</section>
 
+			<!-- begin section 2 -->
 			<section>
 				<h2>My Favorite Things</h2>
 				<ul>
@@ -27,7 +34,10 @@
 				</ul>
 			</section>
 
+			<!-- begin section 3 -->
 			<section>
+
+				<!-- this is a table -->
 				<table>
 					<tr>
 						<th>Column 1 Heading</th>
